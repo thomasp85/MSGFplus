03b79334a4424504557482abc287bc79da46e180
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2014-02-03 13:12:28 +0100

    Check of java version

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,12 @@
 .onLoad <- function(libname, pkgname) {
+    javaVersion <- system2('java', '-version', stderr=T, stdout=T)
+    if(length(javaVersion) == 0) {
+        warning('Java was not found on your system. Java is required for MS-GF+ to work.')
+    } else {
+        javaVersion <- javaVersion[grepl('java version', javaVersion)]
+        as.numeric(sub('.*\"\\d\\.(\\d).*', '\\1', javaVersion))
+        if(as.numeric(sub('.*\"\\d\\.(\\d).*', '\\1', javaVersion)) < 7) {
+            warning('Java need to be at least version 1.7 for MS-GF+ to work. Please upgrade.')
+        }
     }
 }
