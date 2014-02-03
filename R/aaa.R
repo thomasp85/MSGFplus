8b08c56e4dfda12e68bf7833ac52477890f33e6f
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2014-02-03 13:14:58 +0100

    Moved function to aaa.R as it is not a class method

--- R/aaa.R
+++ R/aaa.R
@@ -1,3 +1,22 @@
+#' Create terminal compatible strings of input and output names
+#' 
+#' @param rawfile The location of the spectrum file
+#' 
+#' @param savename The location where the results should be saved
+#' 
+#' @return A character vector that is compatible with the OS that can be concatenated with the MS-GF+ call
+#' 
+createFileCall <- function(rawfile, savename){
+    if(Sys.info()["sysname"] == 'Windows'){
+        rawfile <- paste0('\"', rawfile, '\"')
+        savename <- paste0('\"', savename, '\"')
     } else {
+        rawfile <- gsub(' ', '\\ ', rawfile, fixed=T)
+        savename <- gsub(' ', '\\ ', savename, fixed=T)
     }
+    paste0('-s ', rawfile, ' -o ', savename, ' -d', database)
+}
+
+printMSGFLicense <- function() {
+    writeLines(readLines(R.home(component='library/MSGFplus/inst/MSGFPlus/LICENSE.txt')))
 }
--- R/msgfPar.R
+++ R/msgfPar.R
@@ -240,25 +240,6 @@ setMethod(
   }
 )
 
-#' Create terminal compatible strings of input and output names
-#' 
-#' @param rawfile The location of the spectrum file
-#' 
-#' @param savename The location where the results should be saved
-#' 
-#' @return A character vector that is compatible with the OS that can be concatenated with the MSGF+ call
-#' 
-createFileCall <- function(rawfile, savename){
-  if(Sys.info()["sysname"] == 'Windows'){
-    rawfile <- paste0('\"', rawfile, '\"')
-    savename <- paste0('\"', savename, '\"')
-  } else {
-    rawfile <- gsub(' ', '\\ ', rawfile, fixed=T)
-    savename <- gsub(' ', '\\ ', savename, fixed=T)
-  }
-  paste0('-s ', rawfile, ' -o ', savename, ' -d', database)
-}
-
 #' Constructor for the msgfPar class
 #' 
 #' 
