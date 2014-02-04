ec0218894daa5a09b2f93f30227d2171a1c1f80c
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2014-02-04 15:29:41 +0100

    Stub for package documentation

---
+++ R/MSGFplus-package.R
@@ -0,0 +1,13 @@
+#' An interface between R and MS-GF+
+#' 
+#' This package comes bundled with the peptide database search tool MS-GF+ and
+#' provides means of running peptide searches from within R with automatic
+#' result import and easy handling of parameters.
+#' 
+#' @author Thomas Dybdal Pedersen with contributions from Laurent Gatto
+#' 
+#' @import methods
+#' @docType package
+#' @name mzID-package
+#' 
+NULL
