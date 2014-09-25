6cb8dafdb3389fcbd15f98401a1e21bbfd0cc0a6
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2014-09-25 08:54:02 +0200

    Remove printMSGFLicense as no longer included, T => TRUE

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-    javaVersion <- system2('java', '-version', stderr=T, stdout=T)
+    javaVersion <- system2('java', '-version', stderr=TRUE, stdout=TRUE)
     if(length(javaVersion) == 0) {
         warning('Java was not found on your system. Java is required for MS-GF+ to work.')
     } else {
@@ -9,5 +9,4 @@
             warning('Java need to be at least version 1.7 for MS-GF+ to work. Please upgrade.')
         }
     }
-    printMSGFLicense()
 }
