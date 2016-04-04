f02007f38b0cb8c756a031fcf893f2d42f5d40e1
Dan Tenenbaum <dtenenba@fhcrc.org>
2016-04-04 03:18:40 +0000

    work around path problems in system/system2
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSGFplus@115720 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,22 @@
+
+# from http://stackoverflow.com/a/34031214/470769
+Sys.which2 <- function(cmd) {
+    stopifnot(length(cmd) == 1)
+    if (.Platform$OS.type == "windows") {
+        suppressWarnings({
+            pathname <- shell(sprintf("where %s 2> NUL", cmd), intern=TRUE)[1]
+        })
+        if (!is.na(pathname)) return(setNames(pathname, cmd))
+    }
+    Sys.which(cmd)
+}
+
+.javaExecutable <- function() Sys.which2("java")
+
+
+
 .onLoad <- function(libname, pkgname) {
-    javaVersion <- system2('java', '-version', stderr=TRUE, stdout=TRUE)
+    javaVersion <- system2(.javaExecutable(), '-version', stderr=TRUE, stdout=TRUE)
     if(length(javaVersion) == 0) {
         warning('Java was not found on your system. Java is required for MS-GF+ to work.')
     } else {
